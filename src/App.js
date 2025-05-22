09a659c9e7bf86e39539bbd9ce75f5b61b05abbc
shivani-775 <shivani.krishnakumar2023@vitstudent.ac.in>
2025-05-22 12:56:03 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Sidebar from './components/Sidebar';
+import Header from './components/Header';
+import Overview from './components/Overview';
+import ActivityCards from './components/ActivityCards';
+import FriendsList from './components/FriendsList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Sidebar />
+      <div className="main-content">
+        <Header />
+        <Overview />
+        <ActivityCards />
+      </div>
+      <FriendsList />
     </div>
   );
 }
